7b16e2cf755175ad09b86397053256fd4006d6af
bluestear <bluestear@163.com>
2024-01-10 09:47:28 +0800

    20240110

--- app/admin/version.php
+++ app/admin/version.php
@@ -3,12 +3,12 @@
  * @Author: SonLight Tech
  * @Date: 2023-04-06 16:44:33
  * @LastEditors: light
- * @LastEditTime: 2024-01-03 20:13:05
+ * @LastEditTime: 2024-01-10 09:47:12
  * @Description: SonLight Tech版权所有
  */
 
 return [
-    'version'=>'1.4.1',
-    'release'=>'20240103',
+    'version'=>'1.4.2',
+    'release'=>'20240110',
     'name'=>'Sunphp'
 ];
